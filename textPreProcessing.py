4c6714ee95f3c88706b8fe1ab14010d058270078
bcthakre <bcthakregcp@gmail.com>
2023-09-03 00:46:19 -0500

    Added more methods and made it suitable to run on pandas dataframe

---
+++ textPreProcessing.py
@@ -0,0 +1,196 @@
+import unicodedata
+import string
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+# Download content for lemmenization
+
+import nltk
+nltk.download('wordnet')
+nltk.download('punkt')
+
+# not suitable for data methods in the class
+# class TextProcessor:
+
+#     def __init__(self):
+#         """
+#         Initialize TextProcessor with a lemmatizer and a stemmer
+    
+#         """
+
+#         self.lemmatizer = WordNetLemmatizer()
+#         self.stemmer = PorterStemmer()
+
+
+#     def strip_accents(self,text):
+#         """
+#         Strip accents from input String.
+
+#         Args:
+#         - text (str) : Input text
+
+#         Returns:
+#         - str: Text without accents
+
+#         """
+#         print("Stripping accents...")
+#         return ''.join(c for c in unicodedata.normalize('NFD', text)
+#             if unicodedata.category(c) != 'Mn')
+    
+#     def preprocess(self,sentence):
+#         """
+#         Process a list of sentences sequentially with the following steps
+
+#         1. Lowering
+#         2. Punctuation Removal
+#         3. Trim Whitespaces
+#         4. NFKD normlaization
+#         5. Strip accents
+#         6. Lemmatization
+#         7. Steming
+
+#         Args:
+#         - sentence (str) : List of sentecnes to process
+
+#         Returns:
+
+#         -list : Proceesed Sentecnes
+        
+#         """
+
+#         processed_sentences = []
+
+#         for sentence in sentences:
+#             print("/nProcessing sentence: ", sentence)
+
+#             #1. Lowering
+
+#             print("Lowering...")
+#             sentence = sentence.lower()
+
+#             #2. Punctuation Removal
+#             print("Removing punctuation...")
+#             sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+
+#             #3. Trim whitespaces
+#             print("Trimming whitespaces...")
+#             sentence = sentence.strip()
+
+#             #4. NFKD normalization
+#             print("Applying NFKD Normalizing...")
+#             sentence = unicodedata.normalize('NFKD',sentence)
+
+#             #5. Strip accents
+#             print("Stripping accents...")
+#             sentence = self.strip_accents(sentence)
+
+#             #6 Lemmatization
+#             print("Lemmatizing...")
+#             words = word_tokenize(sentence)
+#             sentence = ' '.join([self.lemmatizer.lemmatize(word) for word in words])
+
+#             #7. Stemming
+#             print("Stemming...")
+#             words = word_tokenize(sentence)
+#             sentence = ' '.join([self.stemmer.stem(word) for word in words])
+
+#             processed_sentences.append(sentence)
+
+#         return processed_sentences
+
+
+class TextProcessor:
+
+    def __init__(self):
+        """
+        Initialize TextProcessor with a lemmatizer and a stemmer
+        """
+        self.lemmatizer = WordNetLemmatizer()
+        self.stemmer = PorterStemmer()
+
+    def strip_accents(self, text):
+        """
+        Strip accents from input String.
+
+        Args:
+        - text (str): Input text
+
+        Returns:
+        - str: Text without accents
+        """
+        return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+
+    def to_lower(self, text):
+        return text.lower()
+
+    def remove_punctuation(self, text):
+        return text.translate(str.maketrans('', '', string.punctuation))
+
+    def trim_whitespace(self, text):
+        return text.strip()
+
+    def normalize_text(self, text):
+        return unicodedata.normalize('NFKD', text)
+
+    def lemmatize_text(self, text):
+        words = word_tokenize(text)
+        return ' '.join([self.lemmatizer.lemmatize(word) for word in words])
+
+    def stem_text(self, text):
+        words = word_tokenize(text)
+        return ' '.join([self.stemmer.stem(word) for word in words])
+
+    def preprocess(self, sentence):
+        """
+        Process a sentence sequentially with the following steps:
+
+        1. Lowering
+        2. Punctuation Removal
+        3. Trim Whitespaces
+        4. NFKD normalization
+        5. Strip accents
+        6. Lemmatization
+        7. Stemming
+
+        Args:
+        - sentence (str): Sentence to process
+
+        Returns:
+        - str: Processed sentence
+        """
+        sentence = self.to_lower(sentence)
+        sentence = self.remove_punctuation(sentence)
+        sentence = self.trim_whitespace(sentence)
+        sentence = self.normalize_text(sentence)
+        sentence = self.strip_accents(sentence)
+        sentence = self.lemmatize_text(sentence)
+        sentence = self.stem_text(sentence)
+
+        return sentence
+
+
+    
+# if __name__ == "__main__":
+#     # Famous sentences from literature and historical figures
+    
+# # Famouse sentences from literature and historical figures
+
+#     sentences = [
+#                 "To be or not to be, that is the question.",  # William Shakespeare
+#                 "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",  # Jane Austen
+#                 "All that glitters is not gold.",  # William Shakespeare
+#                 "That's one small step for man, one giant leap for mankind.",  # Neil Armstrong
+#                 "The only thing we have to fear is fear itself.",  # Franklin D. Roosevelt
+#                 "I think, therefore I am.",  # René Descartes
+#                 "In the beginning was the Word, and the Word was with God, and the Word was God.",  # The Bible
+#                 "It was the best of times, it was the worst of times.",  # Charles Dickens
+#                 "The future belongs to those who believe in the beauty of their dreams."  # Eleanor Roosevelt
+#                 ]
+    
+#     preprocessor = TextProcessor()
+
+#     processed_sentences = preprocessor.preprocess(sentences)
+
+#     for s in processed_sentences:
+#         print("\nProcessed sentence: ", s)
