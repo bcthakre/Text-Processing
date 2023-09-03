b597362dd7b939152c50b1584953a2ce2eba1063
bcthakre <bcthakregcp@gmail.com>
2023-09-03 16:56:15 -0500

    Added more code

--- kaggleIntegration.py
+++ kaggleIntegration.py
@@ -8,7 +8,6 @@ from textPreProcessing import TextProcessor
 
 pd.set_option('display.max_colwidth', None)
 
-
 # intiate the method
 
 proceesor = TextProcessor()
@@ -38,54 +37,3 @@ file_name = os.listdir('.')[5]
 data = pd.read_csv(file_name)
 
 # Replace the NaN values with empty strings
-
-data['Review_Text'].fillna("",inplace=True)
-
-# Use the methods sequentially on the Review_Text column
-
-# Lowering the sentences
-
-data['LoweredSentence'] = data['Review_Text'].apply(proceesor.to_lower)
-
-# strip accents
-
-data['StripedAccents'] = data['Review_Text'].apply(proceesor.strip_accents)
-
-# remove punctuation
-
-data['RemovedPunctuation'] = data['Review_Text'].apply(proceesor.remove_punctuation)
-
-# remove whitespaces
-
-data['RemovedWhitespaces'] = data['Review_Text'].apply(proceesor.trim_whitespace)
-
-# normalize text
-
-data['NormalizedText'] = data['Review_Text'].apply(proceesor.normalize_text)
-
-# lemmatize text
-
-data['LemmatizedText'] = data['Review_Text'].apply(proceesor.lemmatize_text)
-
-# stem text
-# probably the most unuseful one for NLP
-
-data['StemmedText'] = data['Review_Text'].apply(proceesor.stem_text)
-
-# all of the methods above
-
-data['AllPreProcessing'] = data['Review_Text'].apply(proceesor.preprocess)
-
-# applying the Transofmrer and Sentimenal Analysis
-
-from transformers import pipeline
-
-# sentiment Analysis
-
-sentiment_pipeline = pipeline('sentiment-analysis')
-
-# apply the pipeline on the Review_Text column
-
-data['sentiment'] = data['Review_Text'].apply(lambda x: sentiment_pipeline(x)[0]['label'])
-data['sentiment_score'] = data['Review_Text'].apply(lambda x: sentiment_pipeline(x)[0]['score'])
-
