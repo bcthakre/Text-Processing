98070af5ff18f2e4a113bcad6d2d7581bc04dde1
bcthakre <bcthakregcp@gmail.com>
2023-09-10 14:07:44 -0500

    Completed HuggingFace

--- huggingFaceSentiment.py
+++ huggingFaceSentiment.py
@@ -1,44 +1,18 @@
+# Purpose: Sentiment Analysis using Hugging Face
+# Author: Bhupendrasinh Thakre
+
+# import libraries
+
+import pandas as pd
+from transformers import pipeline
+
+# read the csv file
+
+data = pd.read_csv('Hotel_Reviews.csv')
+
+# check the data and replace NAs in the Review_Text column with empty string
 
 data['Review_Text'].fillna("",inplace=True)
-
-# Use the methods sequentially on the Review_Text column
-
-# Lowering the sentences
-
-data['LoweredSentence'] = data['Review_Text'].apply(proceesor.to_lower)
-
-# strip accents
-
-data['StripedAccents'] = data['Review_Text'].apply(proceesor.strip_accents)
-
-# remove punctuation
-
-data['RemovedPunctuation'] = data['Review_Text'].apply(proceesor.remove_punctuation)
-
-# remove whitespaces
-
-data['RemovedWhitespaces'] = data['Review_Text'].apply(proceesor.trim_whitespace)
-
-# normalize text
-
-data['NormalizedText'] = data['Review_Text'].apply(proceesor.normalize_text)
-
-# lemmatize text
-
-data['LemmatizedText'] = data['Review_Text'].apply(proceesor.lemmatize_text)
-
-# stem text
-# probably the most unuseful one for NLP
-
-data['StemmedText'] = data['Review_Text'].apply(proceesor.stem_text)
-
-# all of the methods above
-
-data['AllPreProcessing'] = data['Review_Text'].apply(proceesor.preprocess)
-
-# applying the Transofmrer and Sentimenal Analysis
-
-from transformers import pipeline
 
 # sentiment Analysis
 
